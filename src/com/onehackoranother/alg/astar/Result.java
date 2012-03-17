f1666048ee8843aa4089a56084b51d301c30a9c7
Jason Frame <jason@onehackoranother.com>
2012-03-17 23:38:02 +0000

    and more…

--- src/com/onehackoranother/alg/astar/Result.java
+++ src/com/onehackoranother/alg/astar/Result.java
@@ -2,12 +2,10 @@ package com.onehackoranother.alg.astar;
 
 public class Result<T> {
     Path<T>     path    = null;
-    float       cost    = -1.0f;
-    int         steps   = -1;
+    float       cost;
+    int         steps;
     
     public boolean hasPath() { return path != null; }
-    public boolean hasCost() { return cost >= 0.0f; }
-    public boolean hasStepCount() { return steps >= 0; }
     
     public Path<T> getPath() { return path; }
     public float getCost() { return cost; }
