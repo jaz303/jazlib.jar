d3b10a7fcf1ac0ade91cfb6b7ce01bff64c29234
Jason Frame <jason@onehackoranother.com>
2012-03-26 13:27:02 +0100

    remove physics engine, it's getting its own package

--- src/com/onehackoranother/cyclone/Integrator.java
+++
@@ -1,9 +0,0 @@
-package com.onehackoranother.cyclone;
-
-public class Integrator {
-    public void integrate(Particle[] particles, float delta) {
-        for (int i = 0; i < particles.length; i++) {
-            particles[i].integrate(delta);
-        }
-    }
-}
